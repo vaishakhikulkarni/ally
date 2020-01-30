7d57d00918bd6252bf8acde17bfe26f747191e0f
vaishakhikulkarni <vaishakhikulkarni@gmail.com>
2020-01-29 19:05:20 -0500

    Remove spaces from leading and trailing space

--- src/main/java/SortCharactersFromParagraph.java
+++ src/main/java/SortCharactersFromParagraph.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SortCharactersFromParagraph {
+    public static void main() throws FileNotFoundException {
+        File file = new File("src/main/resources/test.txt");
+        Scanner sc = new Scanner(file);
+
+        StringBuilder inputData = new StringBuilder();
+
+        while (sc.hasNextLine()) {
+            inputData.append(sc.nextLine());
+        }
+
+        String sortedData = sortInputData(inputData.toString());
+        System.out.println("------------------: "+sortedData);
+    }
+
+    public static String sortInputData(String input) {
+        String data = input.trim();
+        return data;
+    }
 }
--- src/test/java/SortCharactersFromParagraphTest.java
+++ src/test/java/SortCharactersFromParagraphTest.java
@@ -1,5 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class SortCharactersFromParagraphTest {
+
+    @Test
+    void inputSingleCharacter_ReturnCharacter() {
+        String input = "t";
+        String actualOutput =SortCharactersFromParagraph.sortInputData(input);
+        Assertions.assertEquals(input,actualOutput);
+    }
+
+    @Test
+    void inputSingleCharacterWithSpace_ReturnCharacterWithoutSpace() {
+        String input = " t ";
+        String actualOutput =SortCharactersFromParagraph.sortInputData(input);
+        Assertions.assertEquals(input.trim(),actualOutput);
+    }
 
 }
